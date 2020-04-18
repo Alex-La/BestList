f1d5523a2428a04182c5ec4a46d2300b60f7e2a6
Alex-La <lars6162@gmail.com>
2020-04-18 17:47:42 +0300

    two routs

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
+
+const pool = mariadb.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  database: "best",
+  connectionLimit: 5,
+});
+
+const app = express();
+
+app.get("/get", async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT * FROM users ORDER BY best_time ASC");
+    res.json(rows);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release();
+  }
+});
+
+app.post("/set", bodyParser.json(), async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const response = await conn.query(
+      `INSERT INTO users (name, best_time) VALUES ('${req.body.name}', '${req.body.best_time}')`
+    );
+    res.status(200).json(response);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release();
+  }
+});
+
+const server = http.createServer(app);
+server.listen(8080, () => console.log("Server listen on port 8080"));
