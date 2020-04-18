6766198bce00619e4eea7e0a7cdf340c5d5910ae
Alex-La <lars6162@gmail.com>
2020-04-18 18:51:20 +0300

    pg added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const http = require("http");
-const mariadb = require("mariadb");
+const pg = require("pg");
 const bodyParser = require("body-parser");
 
-const pool = mariadb.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  database: "best",
-  connectionLimit: 5,
+const Pool = pg.Pool;
+const pool = new Pool({
+  user: "woiydotgaapcda",
+  host: "ec2-54-159-112-44.compute-1.amazonaws.com",
+  database: "d3gs3l95rt24gp",
+  password: "5a8639cca56df406822f98a96bd03003f44b8f4ec0a5fc135265eb983b76c758",
+  port: "5432",
+});
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
 
 const app = express();
@@ -18,15 +25,21 @@ app.get("/", function (req, res) {
 });
 
 app.get("/get", async (req, res) => {
-  let conn;
   try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT * FROM users ORDER BY best_time ASC");
-    res.json(rows);
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) conn.release();
+    pool.connect().then((client) => {
+      return client
+        .query("SELECT * FROM users ORDER BY best_time AS")
+        .then((result) => {
+          client.release();
+          res.json(result.rows);
+        })
+        .catch((e) => {
+          client.release();
+          console.log(e.stack);
+        });
+    });
+  } catch (e) {
+    res.json("Server is closed");
   }
 });
 
